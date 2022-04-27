cc2612b709e6c35b7fd3cc48dfbbb397a192e552
acea68 <alancea01@gmail.com>
2022-04-27 00:59:41 -0500

    finised indexes

--- db/db.sql
+++ db/db.sql
@@ -10,7 +10,8 @@
 -- CREATE INDEX indexStyle ON styles("product_id");
 -- CREATE INDEX indexPhotos ON photos("style_id");
 -- CREATE INDEX indexSkus ON skus("style_id");
-CREATE INDEX indexRelated ON related("product_id");
+-- CREATE INDEX indexRelated ON related("product_id");
+-- CREATE INDEX indexFeatures ON features("product_id");
 
 
 ---------------------------
